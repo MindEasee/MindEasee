0aeaab93b715fe27028c5a7addd37f32d2c2c1de
ghaniarf <fayyaza.160305@gmail.com>
2024-05-19 11:45:27 +0700

    bismillah bento

--- app/src/main/java/org/d3if0041/mopro1/proyekcoba/ui/screen/Tips/Imagery/ImageryScreen.kt
+++ app/src/main/java/org/d3if0041/mopro1/proyekcoba/ui/screen/Tips/Imagery/ImageryScreen.kt
@@ -47,28 +47,34 @@ fun ImageryScreen(navController: NavHostController) {
                         )
                     }
                     Text(
-                        text = "Jalan Kaki Tips",
+                        text = "Guided Imagery Tips",
                         style = MaterialTheme.typography.titleLarge,
                         modifier = Modifier.padding(start = 8.dp)
                     )
                 }
                 Spacer(modifier = Modifier.height(16.dp))
 
+                Text(
+                    "Teknik guided imagery bisa dilakukan kapan pun dan di mana pun, tidak memerlukan peralatan khusus. Hal yang kamu butuhkan hanya tempat yang tenang. Berikut langkah-langkahnya:",
+                    style = MaterialTheme.typography.bodyMedium
+                )
+                Spacer(modifier = Modifier.height(8.dp))
+
                 val tips = listOf(
-                    "1. Buat diri kamu nyaman. Posisi duduk adalah yang terbaik dan tutup mata kamu",
-                    "2. Ambil napas dalam-dalam dan perlahan",
-                    "3. Dengan menggunakan banyak detail sensorik, ciptakan pemandangan yang menenangkan di pikiran kamu. Coba pikirkan sesuatu yang memberi kamu kedamaian, apakah itu tepi danau di rumah masa kecil kamu, jalan setapak yang dilalui di taman favorit kamu atau suasana rindang dan sejuk",
-                    "4. Kembangkan pandangan yang kamu buat sepenuhnya dengan menyertakan suara, bau, dan nuansa udara di kulit kamu",
-                    "5. Bayangkan diri kamu mengembara melalui adegan yang kamu buat, dengan penuh perhatian memperhatikan sekeliling kamu dan memperhatikan setiap detail",
-                    "6. Teruslah bernapas dengan perlahan, biarkan suasana tenang membanjiri kamu dan membantu kamu rileks",
-                    "7. Luangkan 10 hingga 15 menit untuk menikmati gambaran itu",
-                    "8. Akhiri latihan dengan beberapa napas dalam dan buka mata kamu"
+                    "    1. Buat diri kamu nyaman. Posisi duduk adalah yang terbaik dan tutup mata kamu",
+                    "    2. Ambil napas dalam-dalam dan perlahan",
+                    "    3. Dengan menggunakan banyak detail sensorik, ciptakan pemandangan yang menenangkan di pikiran kamu. Coba pikirkan sesuatu yang memberi kamu kedamaian, apakah itu tepi danau di rumah masa kecil kamu, jalan setapak yang dilalui di taman favorit kamu atau suasana rindang dan sejuk",
+                    "    4. Kembangkan pandangan yang kamu buat sepenuhnya dengan menyertakan suara, bau, dan nuansa udara di kulit kamu",
+                    "    5. Bayangkan diri kamu mengembara melalui adegan yang kamu buat, dengan penuh perhatian memperhatikan sekeliling kamu dan memperhatikan setiap detail",
+                    "    6. Teruslah bernapas dengan perlahan, biarkan suasana tenang membanjiri kamu dan membantu kamu rileks",
+                    "    7. Luangkan 10 hingga 15 menit untuk menikmati gambaran itu",
+                    "    8. Akhiri latihan dengan beberapa napas dalam dan buka mata kamu"
                 )
 
                 tips.forEach { tip ->
                     Text(
                         text = tip,
-                        style = MaterialTheme.typography.titleMedium
+                        style = MaterialTheme.typography.bodyMedium
                     )
                     Spacer(modifier = Modifier.height(8.dp))
                 }
